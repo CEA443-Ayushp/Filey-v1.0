8670c1182630209f9cc4ec56d15c62426749c17b
Ayush Prajapati <101302376+CEA443-Ayushp@users.noreply.github.com>
2022-09-03 17:13:23 +0530

    1st commit
    1st commit

---
+++ vfm-admin/_content/users/users.php
@@ -0,0 +1,26 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'filey',
+    'pass' => '$1$xbiA3yu7$2E0tC1gonLnNu/zP49DN9.',
+    'role' => 'superadmin',
+  ),
+  1 => 
+  array (
+    'name' => 'demo',
+    'pass' => '$1$67yvwbLf$wXP71gY3qk1dku2GaUtgV.',
+    'role' => 'user',
+    'quota' => '5120',
+    'dir' => '["demos-directory"]',
+  ),
+  2 => 
+  array (
+    'name' => 'demo2',
+    'pass' => '$1$YDvnHSxf$gIUlvDi7AdWyWmfodGlEb0',
+    'role' => 'user',
+    'dir' => '["demo2-directory"]',
+    'quota' => '1024',
+  ),
+);
